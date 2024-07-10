31252c4afe01b26f0a815c4e7dea4fe6e94e7e9c
Dins <sdinesh960124@gmail.com>
2024-07-10 18:06:19 +0530

    -- centralize error handler

--- globals/app-constants.js
+++ globals/app-constants.js
@@ -1,9 +1,4 @@
 module.exports = {
-    responseStatus : {
-        'error404': 404,
-        'success200': 200,
-        'somethingWentWrong': -1
-    },
     tranactionType : {
         'DEBIT': 'debit',
         'CREDIT': 'credit'
--- index.js
+++ index.js
@@ -18,41 +18,54 @@ app.get("/", (req, res) => {
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 const logger = (req, res, next) => {
-    console.log(req.Method, req.path, "called");
+    console.log(req.method, req.path, "called");
     next();
 }
 app.use(logger)
 
+const handleError = (err, req, res, next) => {
+    console.error(err.stack);
+    res.status(err.statusCode || 500).json({ error: err.message || "Internal Server Error" });
+};
+
+app.use(handleError);
+  
+
+/**
+ * Aceepts Wallet Name and an OPTIONAL initial balance to setup the wallet.
+ * 
+ * It also handles some validation like wallet name is required, and wallet can not be duplicated, and an initial wallet balance cannot be negative
+ * req
+ * { balance, name } e.g. { balance: 20, name: ‘Hello world’ }
+ * 
+ * res
+ * Status 200, Response body: { id, balance, transactionId: ‘4349349843’, name: ‘Hello world’,
+    date: <JS Date obj>}
+ */
 app.post("/setup", async(req, res) => {
-    /**
-     * req
-     * { balance, name } e.g. { balance: 20, name: ‘Hello world’ }
-     * 
-     * res
-     * Status 200, Response body: { id, balance, transactionId: ‘4349349843’, name: ‘Hello world’,
-date: <JS Date obj>}
-     */
     const {balance = 0, name} = req.body;
     if(!name) {
-        res.status(400).send({
-            error: 'Wallet Name is required'
-        })
+        handleError({
+            statusCode: 400,
+            message: 'Wallet name is required'
+        }, null, res)
         return;
     }
 
     if(balance < 0) {
-        res.status(400).send({
-            error: 'Unable to initialize wallet with a negative balance.'
-        })
+        handleError({
+            statusCode: 400,
+            message: 'Unable to initialize wallet with a negative balance.'
+        }, null, res)
         return;
     }
 
-    const existing = await Wallet.find({name: name}).exec();
+    const existing = await Wallet.find({name: name});
     if(existing.length) {
-        res.status(501).send({
-            error: `Duplicate Wallet Name ${name}`,
-            message: 'Please choose a unique wallet name.'
-        })
+        handleError({
+            statusCode: 400,
+            message: `Duplicate wallet name '${name}', Please choose a uniquq wallet name`
+        }, null, res)
         return;
     }
 
@@ -74,7 +87,7 @@ date: <JS Date obj>}
         const transction = new Transaction(initTxn)
 
         transction.save().then((txn) => {
-            res.status(appConstants.responseStatus.success200).send({
+            res.status(200).send({
                 id: wallet._id,
                 balance: wallet.balance,
                 transactionId: txn._id,
@@ -83,9 +96,10 @@ date: <JS Date obj>}
             })
         })
     }).catch(e => {
-        res.status(401).send({
-            error: 'Failed to setup wallet: ' + err
-        })
+        handleError({
+            statusCode: 501,
+            message: 'Failed to setup wallet: ' + err
+        }, null, res)
         return;
     })
 })
@@ -94,39 +108,37 @@ date: <JS Date obj>}
 app.post("/transact/:walletId", async (req, res) => {
     const walletId = req.params.walletId;
     if(!mongoose.Types.ObjectId.isValid(walletId)) {
-        res.status(400).send({
-            error: 'Invalid Wallet Id supplied',
-            message: "Please Supply a valid walletId"
-        });
+        handleError({
+            statusCode: 400,
+            message: 'Invalid Wallet Id supplied'
+        }, null, res)
         return;
     }
 
     const wallet = await Wallet.findById(walletId)
     if(!wallet) {
-        res.status(400).send({
-            error: 'Invalid Wallet Id supplied',
-            message: "Please Supply a valid walletId"
-        });
+        handleError({
+            statusCode: 400,
+            message: 'Invalid Wallet Id supplied'
+        }, null, res)
         return;
     }
 
     const { amount, description } = req.body;
 
     if(!amount) { // this handles both 0 and undefined, null
-        res.status(400).send({
-            error: 'INVALID_AMOUNT',
-            message: "Please supply a non-zero amount"
-        });
+        handleError({
+            statusCode: 400,
+            message: 'Invalid Amount, Please supply a non-zero amount'
+        }, null, res)
         return;
     }
 
     if(amount < 0 && Math.abs(amount) > wallet.balance) {
-        res.status(400).send({
-            error: 'Insufficient Balance',
-            message: "Please maintain a sufficient balance to execute this transaction",
-            bal: wallet.balance // temp remove it
-
-        });
+        handleError({
+            statusCode: 400,
+            message: 'Insufficient Balance, Please maintain a sufficient balance to execute this transaction'
+        }, null, res)
         return;
     }
 
@@ -142,16 +154,15 @@ app.post("/transact/:walletId", async (req, res) => {
 
         // update the wallet with closing balance
         const updatedWallet = await Wallet.findByIdAndUpdate(walletId, {$set: {balance: txn.closingBalance, updatedAt: new Date()}}, {new: true}).exec();
-        // { balance: 30, transactionId: ‘8328832323’ }
         res.status(200).send({
             balance: updatedWallet.balance,
             transactionId: txn._id
         })
     }).catch(err => {
-        res.status(500).send({
-            error: appConstants.responseStatus.somethingWentWrong,
+        handleError({
+            statusCode: 500,
             message: err
-        })
+        }, null, res)
     })
 })
 
@@ -159,11 +170,10 @@ app.get("/transactions", async (req, res) => {
 
     const { walletId, skip = 0, limit = 100 } = req.query;
     if(!mongoose.Types.ObjectId.isValid(walletId)) {
-        res.status(400).send({
-            error: 'Invalid Wallet Id supplied',
-            message: "Please Supply a valid walletId 1ert",
-            is: `${walletId}`
-        });
+        handleError({
+            statusCode: 400,
+            message: 'Invalid Wallet Id supplied'
+        }, null, res)
         return;
     }
 
@@ -171,10 +181,10 @@ app.get("/transactions", async (req, res) => {
     // TODO: only allow to show transactions that match with req.header walletId
 
     if(!walletExist) {
-        res.status(400).send({
-            error: 'UNKNOWN_WALLET',
-            message: "Please supply a valid walletID"
-        });
+        handleError({
+            statusCode: 400,
+            message: 'Invalid Wallet Id supplied'
+        }, null, res)
         return;
     }
 
@@ -211,11 +221,10 @@ app.get("/transactions", async (req, res) => {
 app.get("/wallet/:id", async (req, res) => {
     const { id: walletId } = req.params;
     if(!mongoose.Types.ObjectId.isValid(walletId)) {
-        res.status(400).send({
-            error: 'Invalid Wallet Id supplied',
-            message: "Please Supply a valid walletId 1ert",
-            is: `${walletId}`
-        });
+        handleError({
+            statusCode: 400,
+            message: 'Invalid Wallet Id supplied'
+        }, null, res)
         return;
     }
 
@@ -223,17 +232,15 @@ app.get("/wallet/:id", async (req, res) => {
     // TODO: only allow to show transactions that match with req.header walletId
 
     if(!walletExist) {
-        res.status(400).send({
-            error: 'UNKNOWN_WALLET',
-            message: "Please supply a valid walletID"
-        });
+        handleError({
+            statusCode: 400,
+            message: 'Invalid Wallet Id supplied'
+        }, null, res)
         return;
     }
 
     const { _id: id, balance, name, createdAt: date } = walletExist
     res.status(200).send({id, balance, name, date})
-
-
 })
 
 app.listen(port, () => {
