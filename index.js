cf55b1464dad3353e5b38773bbd5465c728b4a41
Dinesh Sharma <sdinesh960124@gmail.com>
2024-07-10 15:07:07 +0530

    transact API

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const bodyParser = require('body-parser')
 const appConstants = require('./globals/app-constants.js');
 // Database models
 const Transaction = require('./models/transaction.js')
-const Wallet = require('./models/wallet.js')
+const Wallet = require('./models/wallet.js');
+const { default: mongoose } = require('mongoose');
 
 const port = process.env.PORT || 3001;
 
@@ -16,8 +17,13 @@ app.get("/", (req, res) => {
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+const logger = (req, res, next) => {
+    console.log(req.Method, req.path, "called");
+    next();
+}
+app.use(logger)
 
-app.post("/setup", (req, res) => {
+app.post("/setup", async(req, res) => {
     /**
      * req
      * { balance, name } e.g. { balance: 20, name: ‘Hello world’ }
@@ -40,6 +46,17 @@ date: <JS Date obj>}
         })
         return;
     }
+
+    const existing = await Wallet.find({name: name}).exec();
+    if(existing.length) {
+        res.status(501).send({
+            error: `Duplicate Wallet Name ${name}`,
+            message: 'Please choose a unique wallet name.'
+        })
+        return;
+    }
+
+    // since other checks are done we are good to setup the new wallet the given params.
     const now = new Date();
     const newWallet = new Wallet({name, balance, createdAt: now, updatedAt: now})
 
@@ -74,8 +91,68 @@ date: <JS Date obj>}
 })
 
 
-app.post("/transact/:walletId", (req, res) => {
+app.post("/transact/:walletId", async (req, res) => {
+    const walletId = req.params.walletId;
+    if(!mongoose.Types.ObjectId.isValid(walletId)) {
+        res.status(400).send({
+            error: 'Invalid Wallet Id supplied',
+            message: "Please Supply a valid walletId"
+        });
+        return;
+    }
 
+    const wallet = await Wallet.findById(walletId)
+    if(!wallet) {
+        res.status(400).send({
+            error: 'Invalid Wallet Id supplied',
+            message: "Please Supply a valid walletId"
+        });
+        return;
+    }
+
+    const { amount, description } = req.body;
+
+    if(!amount) { // this handles both 0 and undefined, null
+        res.status(400).send({
+            error: 'INVALID_AMOUNT',
+            message: "Please supply a non-zero amount"
+        });
+        return;
+    }
+
+    if(amount < 0 && Math.abs(amount) > wallet.balance) {
+        res.status(400).send({
+            error: 'Insufficient Balance',
+            message: "Please maintain a sufficient balance to execute this transaction",
+            bal: wallet.balance // temp remove it
+
+        });
+        return;
+    }
+
+    // determine credit or debit;
+    const txn = new Transaction({
+        amount: Math.abs(amount),
+        description: description,
+        closingBalance: wallet.balance + amount,
+        type: amount > 0 ? 'credit' : 'debit',
+        executedAt: new Date(),
+        walletId: walletId
+    }).save().then(async (txn) => {
+
+        // update the wallet with closing balance
+        const updatedWallet = await Wallet.findByIdAndUpdate(walletId, {$set: {balance: txn.closingBalance, updatedAt: new Date()}}, {new: true}).exec();
+        // { balance: 30, transactionId: ‘8328832323’ }
+        res.status(200).send({
+            balance: updatedWallet.balance,
+            transactionId: txn._id
+        })
+    }).catch(err => {
+        res.status(500).send({
+            error: appConstants.responseStatus.somethingWentWrong,
+            message: err
+        })
+    })
 })
 
 app.get("/transactions", (req, res) => {
